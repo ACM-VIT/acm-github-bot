8fb09961d637245dd420039cd47005706909ca41
Swarup Kharul <swarupkharulsk@gmail.com>
2021-05-06 16:57:50 +0530

    Initialize: added config files for .eslint and .prettierrc

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const Discord  = require('discord.js')
+const Discord = require('discord.js')
 const client = new Discord.Client()
 const config = require('./config.json')
 const command = require('./command')
 const guildInvites = new Map();
 
 client.on('inviteCreate', async invite => guildInvites.set(invite.guild.id, await invite.guild.fetchInvites()));
-client.on('ready',()=>{
+client.on('ready', () => {
     console.log('Bot is Ready!');
     client.guilds.cache.forEach(guild => {
         guild.fetchInvites()
             .then(invites => guildInvites.set(guild.id, invites))
             .catch(err => console.log(err));
     });
-    command(client,'ping',(message)=>{
+    command(client, 'ping', (message) => {
         message.channel.send('Pong!');
     })
 });
@@ -24,12 +24,12 @@ client.on('guildMemberAdd', async member => {
     try {
         const usedInvite = newInvites.find(inv => cachedInvites.get(inv.code).uses < inv.uses);
         console.log(usedInvite.code)
-        if(usedInvite.code === 'jWuzrT5ZNp'){    
+        if (usedInvite.code === 'jWuzrT5ZNp') {
             let role = member.guild.roles.cache.find(r => r.name === "Haxor");
             member.roles.add(role).catch(console.error);
         }
     }
-    catch(err) {
+    catch (err) {
         console.log(err);
     }
 });
